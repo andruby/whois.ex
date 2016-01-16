a0788afae3b711c1c3ba0488df4f253753e1b07c
Andrew Fecheyr <andrew@bedesign.be>
2016-01-17 00:13:29 +0100

    Match the line with the domain first before using the available_pattern

--- lib/whois.ex
+++ lib/whois.ex
@@ -5,9 +5,12 @@ defmodule Whois do
     {:ok, socket} = :gen_tcp.connect(to_char_list(server.host), 43, [:binary, active: false])
     :ok = :gen_tcp.send(socket, "domain #{hostname}\r\n")
     {:ok, msg} = :gen_tcp.recv(socket, 0)
-    # IO.puts(server.available_pattern)
     # IO.puts(msg)
-    Regex.match?(Regex.compile!(server.available_pattern, "i"), msg)
+    # IO.inspect Regex.run(~r/^.*\Q#{hostname}\E.*$/mi, msg)
+    [line_with_domain] = Regex.run(~r/^.*\Q#{hostname}\E.*$/mi, msg)
+    # IO.puts(server.available_pattern)
+    # IO.puts(line_with_domain)
+    Regex.match?(Regex.compile!(server.available_pattern, "i"), line_with_domain)
   end
 
   def domain_for(hostname) do
